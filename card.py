d2a05d4339215809203021ebc006263bfd2a6b9c
chris-cozy <csande9@clemson.edu>
2022-06-07 15:31:51 -0400

    Created a card class

---
+++ card.py
@@ -0,0 +1,18 @@
+'''
+This file contains the card class.
+'''
+
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
+class Card():
+    '''
+    The card class. The card will contain attributes for suit, rank, and value.
+    '''
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+    
+    def __str__(self):
+        return self.rank + " of " + self.suit
